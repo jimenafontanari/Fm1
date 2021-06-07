1d27b5779d8a25654409fbb986ab6464cf73ef85
Matias Lamela <matiaslamela1992@gmail.com>
2021-06-07 07:37:03 -0300

    queues sin array

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -9,36 +9,49 @@
 // remove():   Head --> null y devuelve 1
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
-function LinkedList() {
+function LinkedList() {}
 
-}
-
-function Node(value){
-
-}
+function Node(value) {}
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
 // Una Hash table contiene un arreglo de "contenedores" o buckets donde puede guardar información.
 // Para este ejercicio, generar 35 buckets para la Hash Table, y realizar los métodos, get, hasKey
 // Para almacenar un valor asociado a una key (string):
-//    - Se pasa ese valor a la función hash(Pista: usar la función charCodeAt), que determina la posición en que debe ir en el arreglo. 
-//    - Luego el elemento se inserta(llamando al método set) en la posición(índice) devuelta. 
+//    - Se pasa ese valor a la función hash(Pista: usar la función charCodeAt), que determina la posición en que debe ir en el arreglo.
+//    - Luego el elemento se inserta(llamando al método set) en la posición(índice) devuelta.
 // Para buscar el valor por su key:
-//    - Sólo habrá que pasarle a la función hash la clave del elemento a buscar y ésta determinará la posición 
+//    - Sólo habrá que pasarle a la función hash la clave del elemento a buscar y ésta determinará la posición
 //      en que se encuentra.
 //    - Usar el número obtenido, para buscar(llamando al método get) el contenedor o bucket donde está el valor.
 //    - Retornar dicho valor.
 
-function HashTable() {
+function HashTable() {}
 
-}
+//Extra credit
+//Implementa la clase Queues sin utilizar arrays!
+/*
+Utilicemos el mismo nodo de las linkedList para poder realizar nuestra Queue!
+*/
 
+/*
+Ahora tenemos que implementar nuestra clase Queue.
+Va a tener las siguientes propiedades:
+head: Es el nodo donde vamos a guardar el primer valor.
+tail: Es el nodo del final de la Queue.
+length: Un integer con el valor del largo de la Queue.
+Va a tener los siguientes metodos:
+enqueue: Debe agregar un nuevo nodo al final y retornar el largo de la queue.
+dequeue: Debe eliminar un nodo y retornar el valor de ese nodo.
+
+*/
+function Queue() {}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-  Node,
-  LinkedList,
-  HashTable
+	Node,
+	Queue,
+	LinkedList,
+	HashTable,
 };
--- 04-EstructuraDeDatos-II/homework/tests/DataStructureII.test.js
+++ 04-EstructuraDeDatos-II/homework/tests/DataStructureII.test.js
@@ -1,7 +1,8 @@
 const {
   Node,
   LinkedList,
-  HashTable
+  HashTable,
+  Queue
 } = require('../homework');
 
 describe('Una linked list', function() {
@@ -174,4 +175,118 @@ describe('HashTable', function() {
     expect(hashTable.hasKey('raboof')).toBe(false);
   });
 
+});
+
+describe('Extracredit: una Queue sin array', function () {
+	var queue;
+
+	beforeEach(function () {
+		queue = new Queue();
+	});
+
+	it('tiene que tener las siguientes propiedades `head` `tail` `length`', function () {
+		expect(queue.hasOwnProperty('head')).toBe(true);
+		expect(queue.hasOwnProperty('tail')).toBe(true);
+		expect(queue.length).toBe(0);
+	});
+
+	it('tiene los metodos `enqueue`, `dequeue`, y `size`', function () {
+		expect(typeof queue.enqueue).toBe('function');
+		expect(typeof queue.dequeue).toBe('function');
+		expect(typeof queue.peek).toBe('function');
+		expect(typeof queue.size).toBe('function');
+	});
+
+	it('tiene size 0 inicialmente', function () {
+		expect(queue.size()).toBe(0);
+	});
+
+	it('incrementa en size cuando agregamos items', function () {
+		queue.enqueue('first in line');
+		expect(queue.size()).toBe(1);
+	});
+
+	it('decrementa en size cuando removemos elementos', function () {
+		queue.enqueue('first');
+		queue.enqueue('second');
+		queue.enqueue('third');
+		queue.dequeue();
+		expect(queue.size()).toBe(2);
+	});
+
+	it('devuelve el item correcto cuando dequeeamos', function () {
+		queue.enqueue('first');
+		queue.enqueue('second');
+		queue.enqueue('third');
+		expect(queue.size()).toBe(3);
+		expect(queue.dequeue()).toBe('first');
+		expect(queue.size()).toBe(2);
+		expect(queue.dequeue()).toBe('second');
+		expect(queue.size()).toBe(1);
+		expect(queue.dequeue()).toBe('third');
+		expect(queue.size()).toBe(0);
+	});
+
+	it('maneja bien el underflow, cuando no hay elementos dequeue devuelve undefined', function () {
+		queue.enqueue('first in line');
+		expect(queue.size()).toBe(1);
+		expect(queue.dequeue()).toBe('first in line');
+		expect(queue.size()).toBe(0);
+		expect(queue.dequeue()).toBe(undefined);
+		expect(queue.size()).toBe(0);
+		expect(queue.dequeue()).toBe(undefined);
+		expect(queue.size()).toBe(0);
+	});
+
+	it('maneja bien varios enqueue y dequeue', function () {
+		queue.enqueue(1);
+		expect(queue.dequeue()).toBe(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		expect(queue.dequeue()).toBe(2);
+		queue.enqueue(4);
+		expect(queue.dequeue()).toBe(3);
+		expect(queue.dequeue()).toBe(4);
+		expect(queue.dequeue()).toBe(undefined);
+	});
+
+	it('agrega y remueve sus propios items', function () {
+		var q2 = new Queue();
+		queue.enqueue('fullstack');
+		q2.enqueue('JavaScript');
+		expect(q2.dequeue()).toBe('JavaScript');
+		expect(q2.dequeue()).toBe(undefined);
+		expect(queue.dequeue()).toBe('fullstack');
+		expect(queue.dequeue()).toBe(undefined);
+	});
+	it('size debe devolver el valor de length', function () {
+		expect(queue.size()).toBe(0);
+		expect(queue.length).toBe(0);
+		queue.enqueue(1);
+		expect(queue.size()).toBe(1);
+		expect(queue.length).toBe(1);
+		queue.enqueue(2);
+		expect(queue.size()).toBe(2);
+		expect(queue.length).toBe(2);
+		queue.enqueue(3);
+		expect(queue.size()).toBe(3);
+		expect(queue.length).toBe(3);
+	});
+	it('peek debe mostrar el valor del head', function () {
+		var q2 = new Queue();
+		q2.enqueue(1);
+		expect(q2.peek()).toBe(1);
+		expect(q2.dequeue()).toBe(1);
+		q2.enqueue(2);
+		expect(q2.peek()).toBe(2);
+		q2.enqueue(3);
+		expect(q2.peek()).toBe(2);
+		expect(q2.dequeue()).toBe(2);
+		q2.enqueue(4);
+		expect(q2.peek()).toBe(3);
+		expect(q2.dequeue()).toBe(3);
+		expect(q2.peek()).toBe(4);
+		expect(q2.dequeue()).toBe(4);
+		expect(q2.dequeue()).toBe(undefined);
+	});
 });
